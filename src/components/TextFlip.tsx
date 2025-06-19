910b399040ec4b3def8e042863498cfab76b10eb
theCalculatar <alpheusm13@gmail.com>
2025-06-20 00:02:35 +0200

    fix flipper disapering

--- src/components/TextFlip.tsx
+++ src/components/TextFlip.tsx
@@ -11,25 +11,29 @@ function TextFlip({
       <div
         className={`ext-flip-container relative overflow-hidden ${className}`}
       >
-        <div className="text-flip flex flex-col transition-transform duration-300 ease-in-out cursor-pointer hover:-translate-y-4 -mt-0.5">
+        <div className="text-flip flex flex-col sm: transition-transform duration-300 ease-in-out cursor-pointer sm:hover:-translate-y-4 active:hover:-translate-y-4 -mt-0.5">
           {children}
         </div>
       </div>
       <style>{`
-        .text-flip:hover .initial {
-          display: none;
-        }
-        .text-flip:hover .preview {
-          transform: translateY(1rem);
-          display: block;
-        }
-        .text-flip:active .initial {
-          display: none;
-        }
-        .text-flip:active .preview {
-          transform: translateY(1rem);
-          display: block;
-        }
+.text-flip:active:hover .initial {
+  display: none;
+}
+.text-flip:active:hover .preview {
+  transform: translateY(1rem);
+  display: block;
+}
+
+
+@media screen and (width>=640) {
+  .text-flip:hover .initial {
+    display: none;
+  }
+  .text-flip:hover .preview {
+    transform: translateY(1rem);
+    display: block;
+  }
+}
       `}</style>
     </div>
   );
